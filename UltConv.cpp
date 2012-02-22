79a0ce583b5f5f5c83af67c10929f9c322b8639c
Tyrone <taoabc@gmail.com>
2012-02-23 00:09:52 +0800

    rename UltEncoding to UltConv

---
+++ UltConv.cpp
@@ -0,0 +1,54 @@
+#include "StdAfx.h"
+#include "UltConv.h"
+#include <Windows.h>
+
+CStringW Ult::Utf8ToUnicode(const char* src, int len)
+{
+	int l = ::MultiByteToWideChar(CP_UTF8, 0, src, len, NULL, 0);
+	CStringW str;
+	::MultiByteToWideChar(CP_UTF8, 0, src, len, str.GetBuffer(l), l);
+	str.ReleaseBuffer();
+	return str;
+}
+
+CStringA Ult::UnicodeToUtf8(const wchar_t* src, int len)
+{
+	int l = ::WideCharToMultiByte(CP_UTF8, 0, src, len, NULL, 0, NULL, NULL);
+	CStringA str;
+	::WideCharToMultiByte(CP_UTF8, 0, src, len, str.GetBuffer(l), l, NULL, NULL);
+	str.ReleaseBuffer();
+	return str;
+}
+
+CStringA Ult::UnicodeToUtf8(const wchar_t* src)
+{
+	return UnicodeToUtf8(src, wcslen(src)+1);
+}
+
+CStringW Ult::AnsiToUnicode(const char* src, int len)
+{
+	int l = ::MultiByteToWideChar(CP_ACP, 0, src, len, NULL, 0);
+	CStringW str;
+	::MultiByteToWideChar(CP_ACP, 0, src, strlen(src)+1, str.GetBuffer(l), l);
+	str.ReleaseBuffer();
+	return str;
+}
+
+CStringW Ult::AnsiToUnicode(const char* src)
+{
+	return AnsiToUnicode(src, strlen(src)+1);
+}
+
+CStringA Ult::UnicodeToAnsi(const wchar_t* src, int len)
+{
+	int l = ::WideCharToMultiByte(CP_ACP, 0, src, len, NULL, 0, NULL, NULL);
+	CStringA str;
+	::WideCharToMultiByte(CP_ACP, 0, src, len, str.GetBuffer(l), l, NULL, NULL);
+	str.ReleaseBuffer();
+	return str;
+}
+
+CStringA Ult::UnicodeToAnsi(const wchar_t* src)
+{
+	return UnicodeToAnsi(src, wcslen(src) + 1);
+}
