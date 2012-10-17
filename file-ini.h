bf2879a412e3149738de98f2be867e7f516bdce6
Tyrone <taoabc@gmail.com>
2012-10-17 19:35:51 +0800

    adjust ini

---
+++ file-ini.h
@@ -0,0 +1,12 @@
+/*
+** simple header include
+** author
+**   taoabc@gmail.com
+*/
+
+#ifndef ULT_FILEINI_H_
+#define ULT_FILEINI_H_
+
+#include "./file/file-ini.h"
+
+#endif
--- file/file-ini.h
+++ file/file-ini.h
@@ -3,8 +3,8 @@
 ** author
 **   taoabc@gmail.com
 */
-#ifndef ULT_FILE_INI_H_
-#define ULT_FILE_INI_H_
+#ifndef ULT_FILE_FILEINI_H_
+#define ULT_FILE_FILEINI_H_
 
 #include <string>
 #include <Windows.h>
