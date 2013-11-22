e6af7bd08b83bf56e0554ee537423106dce64270
Tony Hu <taoabc@gmail.com>
2013-11-22 23:28:44 +0800

    + add operator== + add Get funciton

--- comptr.h
+++ comptr.h
@@ -52,7 +52,11 @@ public:
     }
   }
 
-  operator T*() const {
+  operator T*(void) const {
+    return static_cast<T*>(p_);
+  }
+
+  T* Get(void) const {
     return static_cast<T*>(p_);
   }
 
@@ -107,6 +111,12 @@ private:
 
   T* p_;
 }; // class ComPtr
+
+template <typename T1, typename T2>
+bool operator==(const ComPtr<T1>& p1, const ComPtr<T2>& p2) {
+  return p1.Get() == p2.Get();
+}
+
 } //namespace ult
 
 #endif
